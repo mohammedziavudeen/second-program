0afe8613977a91ee5383a570f0ea294b1bf11087
mohammedziavudeen <mdziavu443@gmail.com>
2023-01-30 16:25:51 +0530

    first program

---
+++ theatre.java
@@ -0,0 +1,20 @@
+class theatre
+{
+static String name = "Kasi";
+String movie;
+int ticket_price;
+public static void main(String[] args)
+{
+ theatre screen1 = new theatre();
+ theatre screen2 = new theatre();
+ theatre screen3 = new theatre();
+	screen1.movie="Varisu";
+	screen2.movie="Thunivu";
+	System.out.println(screen2.movie+" "+screen1.movie+" "+screen3.movie);
+}
+	/*theatre()
+ {
+	System.out.println("pop corn");
+ }*/
+}	
+
